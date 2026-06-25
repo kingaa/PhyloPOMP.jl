eb7fffc924ea87af54f48e70965172dfa792e45d
Aaron A. King <kingaa@umich.edu>
2026-06-25 14:41:25 -0400

    streamline seir example; include destructive sampling

--- src/examples/seir_naive.jl
+++ src/examples/seir_naive.jl
@@ -22,21 +22,23 @@ include("seir_trees.jl")
 
 seir_singular!(
     cols, geneal, node, ll;
-    S, E, I, R, pop, β, ψ,
+    S, E, I, R, pop, β, ψ, χ,
     _...,
 ) = begin
-    (ellE,ellI) = ell(cols)
-    @assert I ≥ ellI && E ≥ ellE
+    ellE, ellI = ell(cols)
     n = geneal[node]
+    @assert I ≥ ellI && E ≥ ellE
     if n.type==Root
         if length(n.children) == 1
             if E-ellE+I-ellI > 0
                 i, _, p = rcateg([E-ellE, I-ellI], DemeSet, true)
                 ll -= log(p)
-                (ellE,ellI) = plant!(cols,i,n.lineage)
+                ellE, ellI = plant!(cols,i,n.lineage)
             else
+                ## even though this realization is incompatible with the data,
+                ## it is necessary to correct the coloring to avoid downstream errors.
                 ll += Prob(-Inf)
-                (ellE,ellI) = plant!(cols,Infec,n.lineage)
+                ellE, ellI = plant!(cols,Infec,n.lineage)
                 I += 1
             end
         else
@@ -44,48 +46,51 @@ seir_singular!(
         end
     elseif n.type==Sample
         if n.lineage ∉ cols[Infec]
+            ## even though this realization is incompatible with the data,
+            ## it is necessary to correct the coloring to avoid downstream errors.
             ll += Prob(-Inf)
-            (ellE,ellI) = swap!(cols,Expos,Infec,n.lineage)
+            ellE, ellI = swap!(cols,Expos,Infec,n.lineage)
             E -= 1
             I += 1
         end
-        ll += log(ψ*I)
         if length(n.children) == 0
-            (ellE,ellI) = chop!(cols,Infec,n.lineage)
-            ll += log(1-ellI/I)
+            ll += log((ψ+χ)*I)
+            ellE, ellI = chop!(cols,Infec,n.lineage)
+            k,_ = rcateg([ψ, χ])
+            if k==1             # non-destructive sample
+                ll += log(1-ellI/I)
+            elseif k==2         # destructive sample
+                I -= 1
+            else
+                @assert false "impossible choice" # COV_EXCL_LINE
+            end
         elseif length(n.children) == 1
-            (ellE,ellI) = chop!(cols,Infec,n.lineage,Infec,geneal[n.children[1]].lineage)
-            ll -= log(I)
+            chillin = geneal[n.children[1]].lineage
+            ellE, ellI = chop!(cols,Infec,n.lineage,Infec,chillin)
+            ll += log(ψ)
         else
             error("too many children ($(length(n.children)) > 1) at sample $(n.name), t=$(n.time)")
         end
     elseif n.type==Node
         if n.lineage ∉ cols[Infec]
+            ## even though this realization is incompatible with the data,
+            ## it is necessary to correct the coloring to avoid downstream errors.
             ll += Prob(-Inf)
-            (ellE,ellI) = swap!(cols,Expos,Infec,n.lineage)
+            ellE, ellI = swap!(cols,Expos,Infec,n.lineage)
             E -= 1
             I += 1
         end
         if length(n.children) == 2
+            chillins = map(n.children) do i
+                geneal[i].lineage
+            end
             ll += log(β*S*I/pop)
             k, _, p = rcateg([1, 1], true)
             ll -= log(p)
             if k==1
-                (ellE,ellI) = fork!(
-                    cols,
-                    Infec,n.lineage,
-                    (Expos,Infec),
-                    (geneal[n.children[1]].lineage,
-                     geneal[n.children[2]].lineage)
-                )
+                ellE, ellI = fork!(cols,Infec,n.lineage,(Expos,Infec),chillins)
             else
-                (ellE,ellI) = fork!(
-                    cols,
-                    Infec,n.lineage,
-                    (Expos,Infec),
-                    (geneal[n.children[2]].lineage,
-                     geneal[n.children[1]].lineage)
-                )
+                ellE, ellI = fork!(cols,Infec,n.lineage,(Infec,Expos),chillins)
             end
             S -= 1
             E += 1
@@ -94,18 +99,18 @@ seir_singular!(
             error("too many children ($(length(n.children)) ≠ 2) at node $(n.name), t=$(n.time)")
         end
     else
-        @assert false "impossible node type"
+        @assert false "impossible node type" # COV_EXCL_LINE
     end
-    (; ll = ll, S = S, E = E, I = I, R = R)
+    ll, S, E, I, R
 end
 
 event_rates!(
     alpha, pi, cols;
     S, E, I, R,
-    β, σ, γ, ω, ψ, pop,
+    β, σ, γ, ω, ψ, χ, pop,
     _...,
 ) = begin
-    (ellE,ellI) = ell(cols)
+    ellE, ellI = ell(cols)
     @assert I ≥ ellI && E ≥ ellE
     alpha[2] = alpha[1] = β*S*I/pop
     alpha[4] = alpha[3] = σ*E
@@ -116,26 +121,26 @@ event_rates!(
     pi[3] = @indicator(E > 0, 1-ellE/E)
     pi[4] = @indicator(E > 0, ellE/E)
     pi[6] = pi[5] = 1.0
-    ψ*I + γ*ellI + @indicator(I ≤ ellI, γ*(I-ellI))
+    ψ*I + χ*I + γ*ellI + @indicator(I ≤ ellI, γ*(I-ellI))
 end
 
 seir_regular!(
     cols, ll;
     t, dt,
     S, E, I, R,
-    β, σ, γ, ω, ψ, pop,
+    β, σ, γ, ω, ψ, χ, pop,
     _...,
 ) = begin
     tf = t+dt
     if t < tf
         alpha = similar(Vector{Prob}, 6)
         pi = similar(Vector{Prob}, 6)
-        (ellE,ellI) = ell(cols)
-        @assert I ≥ ellI && E ≥ ellE
+        ellE, ellI = ell(cols)
         decay = event_rates!(
             alpha, pi, cols;
             S = S, E = E, I = I, R = R,
-            β = β, σ = σ, γ = γ, ω = ω, ψ = ψ, pop = pop,
+            β = β, σ = σ, γ = γ, ω = ω,
+            ψ = ψ, χ = χ, pop = pop,
         )
         k, s = rcateg(alpha .* pi)
         step::Time = -log(rand())/s
@@ -148,7 +153,7 @@ seir_regular!(
             elseif k==2
                 ll += log(ellI)
                 b = rand(cols[Infec])
-                (ellE,ellI) = swap!(cols,Infec,Expos,b)
+                ellE, ellI = swap!(cols,Infec,Expos,b)
                 S -= 1
                 E += 1
                 ll += log(1-ellI/I)-log(E)
@@ -159,7 +164,7 @@ seir_regular!(
             elseif k==4
                 ll += log(ellE)
                 b = rand(cols[Expos])
-                (ellE,ellI) = swap!(cols,Expos,Infec,b)
+                ellE, ellI = swap!(cols,Expos,Infec,b)
                 E -= 1
                 I += 1
                 ll -= log(I)
@@ -171,14 +176,14 @@ seir_regular!(
                 R -= 1
                 S += 1
             else
-                @assert false "impossible error!"
+                @assert false "impossible error!" # COV_EXCL_LINE
             end
-            @assert I ≥ ellI && E ≥ ellE
             t += step
             decay = event_rates!(
                 alpha, pi, cols;
                 S = S, E = E, I = I, R = R,
-                β = β, σ = σ, γ = γ, ω = ω, ψ = ψ, pop = pop,
+                β = β, σ = σ, γ = γ, ω = ω,
+                ψ = ψ, χ = χ, pop = pop,
             )
             k, s = rcateg(alpha .* pi)
             step = -log(rand())/s
@@ -187,11 +192,11 @@ seir_regular!(
         ll -= decay*step
     end
     @assert I ≥ ellI && E ≥ ellE
-    (; ll = ll, S = S, E = E, I = I, R = R)
+    ll, S, E, I, R
 end
 
 """
-    seir(gen; β = 4.0, σ = 1.0, γ = 1.0, ω = 1.0, ψ = 0.02,
+    seir(gen; β = 4.0, σ = 1.0, γ = 1.0, ω = 1.0, ψ = 0.02, χ = 0.0,
          pop = 100, S0 = 0.9, E0 = 0.0, I0 = 0.02, R0 = 0.08)
 
 Constructs a pomp object based on the genealogy `gen` and implementing
@@ -199,14 +204,14 @@ the naïve genealogical filter.
 """
 seir(
     gen::Genealogy;
-    β = 4.0, σ = 1.0, γ = 1.0, ω = 1.0, ψ = 0.02,
+    β = 4.0, σ = 1.0, γ = 1.0, ω = 1.0, ψ = 0.02, χ = 0.0,
     pop = 100,
     S0 = 0.9, E0 = 0.0, I0 = 0.02, R0 = 0.08,
 ) = begin
     pomp(
         params = (
             β = Float64(β), σ = Float64(σ), γ = Float64(γ),
-            ω = Float64(ω), ψ = Float64(ψ),
+            ω = Float64(ω), ψ = Float64(ψ), χ = Float64(χ),
             pop = Float64(pop),
             S0 = Float64(S0), E0 = Float64(E0),
             I0 = Float64(I0), R0 = Float64(R0),
